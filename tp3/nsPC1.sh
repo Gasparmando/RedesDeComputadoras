4682b778f4361ec2191e1d6af27ce428f8dc20a5
gasparmando <gasparmando@hotmail.com>
2018-06-08 23:38:10 -0300

    Subo todas las carpetas

--- tp3/nsPC1.sh
+++ tp3/nsPC1.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
-./nsclear.sh
 
-NS1="ns2.1"
-NS2="ns2.2"
-NS3="ns2.3"
-NS4="ns2.4"
-NS5="ns2.5"
-NS6="ns2.6"
+#NAMESPACES
+NS1="ns1.1"
+NS2="ns1.2"
+NS3="ns1.3"
+NS4="ns1.4"
+NS5="ns1.5"
+NS6="ns1.6"
 
+#INTERFAZ
+IFFISICA="enp0s3"
 
+#Bridge Externo
+BRNAME="br-externo"
 
+#Direcciones
+
+tap1B="2001:aaaa:aaaa:1::1"
+br="2001:aaaa:aaaa:1::10"
+
+tap12="2001:aaaa:1111:1::12"
+tap21="2001:aaaa:1111:1::21"
+
+tap13="2001:aaaa:2222:1::13"
+tap31="2001:aaaa:2222:1::31"
+
+tap14="2001:aaaa:3333:1::14"
+tap41="2001:aaaa:3333:1::41"
+
+tap34="2001:aaaa:6666:1::34"
+tap43="2001:aaaa:6666:1::43"
+
+tap35="2001:aaaa:4444:1::35"
+tap53="2001:aaaa:4444:1::53"
+
+tap64="2001:aaaa:5555:1::64"
+tap46="2001:aaaa:5555:1::46"
+
+net1="2001:aaaa:1111:1::"
+net2="2001:aaaa:2222:1::"
+net3="2001:aaaa:3333:1::"
+net4="2001:aaaa:4444:1::"
+net5="2001:aaaa:5555:1::"
+net6="2001:aaaa:6666:1::"
+netBr="2001:aaaa:aaaa:1::"
 
 # Crea un nuevo namespace
 ip netns add $NS1
@@ -17,9 +51,8 @@ ip netns add $NS2
 ip netns add $NS3
 ip netns add $NS4
 ip netns add $NS5
-ip netns add $NS6	
+ip netns add $NS6
 
-					
 # Levanta la interfaz de loopback (UP). "exec" para ejecutar dentro del namespace
 ip netns exec $NS1 ip link set up dev lo 
 ip netns exec $NS2 ip link set up dev lo 
@@ -29,7 +62,7 @@ ip netns exec $NS5 ip link set up dev lo
 ip netns exec $NS6 ip link set up dev lo 			
 
 # Crea el cable virtual
-ip link add tap1B type veth peer name br-tap1B 			
+ip link add tap1B type veth peer name br-tap			
 #ip link add tapExt type veth peer name br-tapExt
 
 ip link add tap21 type veth peer name tap12
@@ -61,23 +94,23 @@ ip link set tap64 netns $NS6
 
 
 # Crea el Bridge
-brctl addbr br-externo						
+brctl addbr $BRNAME				
 
 
 # Conecta el cable al Bridge
-brctl addif br-externo br-tap1B					
+brctl addif $BRNAME br-tap				
 #brctl addif br-externo br-tapExt
-brctl addif br-externo enp0s3
+brctl addif $BRNAME $IFFISICA
 
 # Configura ip Forwarding en el router, dentro del NS (1 a 4 son routers) ( 5 y 6 son hosts)
 ip netns exec $NS1 sysctl -w net.ipv6.conf.all.forwarding=1
 ip netns exec $NS2 sysctl -w net.ipv6.conf.all.forwarding=1	
 ip netns exec $NS3 sysctl -w net.ipv6.conf.all.forwarding=1	
 ip netns exec $NS4 sysctl -w net.ipv6.conf.all.forwarding=1	
-	
+
 # Levanta las interfaces de los cables
-ip netns exec $NS1 ip link set up dev tap1B
-ip netns exec $NS1 ip link set dev tap1B mtu 1300 			
+ip netns exec $NS1 ip link set up dev tap1B	
+#ip netns exec $NS1 ip link set dev tap1B mtu 1300 		
 ip netns exec $NS1 ip link set up dev tap12
 ip netns exec $NS1 ip link set up dev tap13
 ip netns exec $NS1 ip link set up dev tap14
@@ -96,51 +129,19 @@ ip netns exec $NS5 ip link set up dev tap53
 
 ip netns exec $NS6 ip link set up dev tap64
 
-ip link set up dev br-tap1B
-#ip link set up dev br-tapExt
-#ip link set up dev tapExt
-ip link set up dev br-externo
+ip link set up dev br-tap
+
+ip link set up dev $BRNAME
 
 
-
-
-
-
-# FORMATO : 2001:{maquina}:{enlace}:1::{interfaz tap}    maquina: (aaaa, bbbb)  enlace: ( 1,2,3,4,5  - A,B,C)
-#Asignacion de direcciones IPv6
-
-tap1B="2001:aaaa:aaaa:1::1A"
-brtap1b="2001:aaaa:aaaa:1::A1"
-br="2001:cccc:cccc:1::A"
-
-
-
-tap12="2001:bbbb:1111:1::12"
-tap21="2001:bbbb:1111:1::21"
-
-tap13="2001:bbbb:2222:1::13"
-tap31="2001:bbbb:2222:1::31"
-
-tap14="2001:bbbb:3333:1::14"
-tap41="2001:bbbb:3333:1::41"
-
-tap34="2001:bbbb:6666:1::34"
-tap43="2001:bbbb:6666:1::43"
-
-tap35="2001:bbbb:4444:1::35"
-tap53="2001:bbbb:4444:1::53"
-
-tap64="2001:bbbb:5555:1::64"
-tap46="2001:bbbb:5555:1::46"
-
-
-ip -6 addr add ${br}/64 dev br-externo
+#ASIGNACION DE DIRECCIONES IPv6
+ip -6 addr add $br/64 dev $BRNAME
 
 ip netns exec $NS1 ip -6 addr add ${tap1B}/64 dev tap1B 
 ip netns exec $NS1 ip -6 addr add ${tap12}/64 dev tap12
 ip netns exec $NS1 ip -6 addr add ${tap13}/64 dev tap13 
-ip netns exec $NS1 ip -6 addr add ${tap14}/64 dev tap14 
-ip netns exec $NS1 ip set dev tap1B mtu 1300
+ip netns exec $NS1 ip -6 addr add ${tap14}/64 dev tap14
+
 
 ip netns exec $NS2 ip -6 addr add ${tap21}/64 dev tap21
 ip netns exec $NS3 ip -6 addr add ${tap31}/64 dev tap31
@@ -156,36 +157,26 @@ ip netns exec $NS5 ip -6 addr add ${tap53}/64 dev tap53
 ip netns exec $NS6 ip -6 addr add ${tap64}/64 dev tap64
 
 
-
 ##Ruteo estático
 
 #para ver las tablas de ruteo:
 # ip netns exec ns1.1 ip -6 route
 
-ip netns exec $NS1 ip -6 route add default via $brtap1b
-#ip netns exec $NS1 ip -6 route add 2001:aaaa:4444:1::/64 dev tap13
-#ip netns exec $NS1 ip -6 route add 2001:aaaa:5555:1::/64 dev tap14
-#ip netns exec $NS1 ip -6 route add 2001:aaaa:6666:1::/64 dev tap13
-ip netns exec $NS1 ip -6 route add 2001:bbbb:4444:1::/64 dev tap13 via 2001:bbbb:2222:1::31
-ip netns exec $NS1 ip -6 route add 2001:bbbb:5555:1::/64 dev tap14 via 2001:bbbb:3333:1::41
-ip netns exec $NS1 ip -6 route add 2001:bbbb:6666:1::/64 dev tap13 via 2001:bbbb:2222:1::31
+ip netns exec $NS1 ip -6 route add default via 2001:aaaa:aaaa:1::2
+ip netns exec $NS1 ip -6 route add $net4/64 via $tap31
+ip netns exec $NS1 ip -6 route add $net5/64 via $tap41
+ip netns exec $NS1 ip -6 route add $net6/64 via $tap31
 
 ip netns exec $NS2 ip -6 route add default via $tap12
 
 ip netns exec $NS3 ip -6 route add default via $tap13
-ip netns exec $NS3 ip -6 route add 2001:bbbb:5555:1::/64 dev tap34 via 2001:bbbb:6666:1::43
+ip netns exec $NS3 ip -6 route add $net5/64 via $tap43
 
 ip netns exec $NS4 ip -6 route add default via $tap14
-ip netns exec $NS4 ip -6 route add 2001:bbbb:4444:1::/64 dev tap43 via 2001:bbbb:6666:1::34
+ip netns exec $NS4 ip -6 route add $net4/64 via $tap34
 
 ip netns exec $NS5 ip -6 route add default via $tap35
 ip netns exec $NS6 ip -6 route add default via $tap46
-
-
-
-
-
-
 
 
 
