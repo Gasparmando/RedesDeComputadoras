348fa54f3997780f80aeef894ad8bf173bc82bf2
gasparmando <gasparmando@hotmail.com>
2018-06-16 17:49:33 -0300

    Version andando

---
+++ tp7/PC1/node/generate-stackbrew-pr.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+
+set -e
+. functions.sh
+
+if [ -z "${1}" ]; then
+  COMMIT_ID="${TRAVIS_COMMIT}"
+  COMMIT_MESSAGE="${TRAVIS_COMMIT_MESSAGE}"
+  BRANCH_NAME="travis-${TRAVIS_BUILD_ID}"
+  GITHUB_USERNAME="nodejs-github-bot"
+else
+  COMMIT_ID="${1}"
+  COMMIT_MESSAGE="$(git show -s --format=%B "${COMMIT_ID}")"
+  BRANCH_NAME="travis-$(date +%s)"
+  if [[ "$(git remote get-url origin)" =~ github.com/([^/]*)/docker-node.git ]]; then
+    GITHUB_USERNAME="${BASH_REMATCH[1]}"
+  fi
+fi
+
+if [[ "${COMMIT_MESSAGE}" =~ Merge\ pull\ request\ \#([0-9]*) ]]; then
+
+  # This is a merge from a pull request
+  PR_NUMBER="${BASH_REMATCH[1]}"
+  COMMIT_MESSAGE="$(printf "%s" "${COMMIT_MESSAGE}" | tail -n 1)"
+fi
+
+IMAGES_FILE="library/node"
+REPO_NAME="official-images"
+ORIGIN_SLUG="${GITHUB_USERNAME}/${REPO_NAME}"
+UPSTREAM_SLUG="docker-library/${REPO_NAME}"
+DOCKER_SLUG="nodejs/docker-node"
+gitpath="../${REPO_NAME}"
+
+function auth_header() {
+  echo "Authorization: token ${GITHUB_API_TOKEN}"
+}
+
+function permission_check() {
+  if [ -z "${GITHUB_API_TOKEN}" ]; then
+    fatal "Environment variable \$GITHUB_API_TOKEN is missing or empty"
+  fi
+
+  auth="$(curl -H "$(auth_header)" \
+    -s \
+    "https://api.github.com")"
+
+  if [ "$(echo "${auth}" | jq -r .message)" = "Bad credentials" ]; then
+    fatal "Authentication Failed! Invalid \$GITHUB_API_TOKEN"
+  fi
+
+  auth="$(curl -H "$(auth_header)" \
+    -s \
+    "https://api.github.com/repos/${ORIGIN_SLUG}/collaborators/${GITHUB_USERNAME}/permission")"
+  if [ "$(echo "${auth}" | jq -r .message)" != "null" ]; then
+    fatal "\$GITHUB_API_TOKEN can't push to https://github.com/${ORIGIN_SLUG}.git"
+  fi
+}
+
+function setup_git_author() {
+  GIT_AUTHOR_NAME="$(git show -s --format="%aN" "${COMMIT_ID}")"
+  GIT_AUTHOR_EMAIL="$(git show -s --format="%aE" "${COMMIT_ID}")"
+  GIT_COMMITTER_NAME="$(git show -s --format="%cN" "${COMMIT_ID}")"
+  GIT_COMMITTER_EMAIL="$(git show -s --format="%cN" "${COMMIT_ID}")"
+
+  export GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL GIT_COMMITTER_NAME GIT_COMMITTER_EMAIL
+}
+
+function message() {
+  echo "Node: ${COMMIT_MESSAGE}"
+}
+
+function pr_payload() {
+  local escaped_message
+  escaped_message="$(echo "${COMMIT_MESSAGE}" | sed -E -e "s/\"/\\\\\"/g")"
+  echo "{
+    'title': 'Node: ${escaped_message}',
+    'body': 'Commit: nodejs/docker-node@${COMMIT_ID}',
+    'head': '${GITHUB_USERNAME}:${BRANCH_NAME}',
+    'base': 'master'
+  }"
+}
+
+function comment_payload() {
+  local pr_url
+  pr_url="${1}"
+  echo "{
+    'body': 'Created PR to the ${REPO_NAME} repo (${pr_url})'
+  }"
+}
+
+if images_updated "${COMMIT_ID}"; then
+
+  permission_check
+
+  # Set Git User Info
+  setup_git_author
+
+  info "Cloning..."
+  git clone --depth 50 "https://github.com/${UPSTREAM_SLUG}.git" ${gitpath} 2>/dev/null
+
+  stackbrew="$(./generate-stackbrew-library.sh)"
+
+  cd ${gitpath}
+
+  echo "${stackbrew}" >"${IMAGES_FILE}"
+  git checkout -b "${BRANCH_NAME}"
+  git add "${IMAGES_FILE}"
+  git commit -m "$(message)"
+
+  info "Pushing..."
+  git push "https://${GITHUB_API_TOKEN}:x-oauth-basic@github.com/${ORIGIN_SLUG}.git" -f "${BRANCH_NAME}" 2>/dev/null || fatal "Error pushing the updated stackbrew"
+
+  cd - && rm -rf ${gitpath}
+
+  info "Creating Pull request"
+  pr_response_payload="$(curl -H "$(auth_header)" \
+    -s \
+    -X POST \
+    -d "$(pr_payload)" \
+    "https://api.github.com/repos/${UPSTREAM_SLUG}/pulls")"
+
+  url="$(echo "${pr_response_payload}" | jq -r .html_url)"
+  if [ "${url}" != "null" ]; then
+    info "Pull request created at ${url}"
+
+    if [ ! -z "${PR_NUMBER}" ]; then
+      comment_endpoint="https://api.github.com/repos/${DOCKER_SLUG}/issues/${PR_NUMBER}/comments"
+    else
+      comment_endpoint="https://api.github.com/repos/${DOCKER_SLUG}/commits/${COMMIT_ID}/comments"
+    fi
+
+    info "Creating Commit Comment"
+    commit_response_payload="$(curl -H "$(auth_header)" \
+      -s \
+      -X POST \
+      -d "$(comment_payload "${url}")" \
+      "${comment_endpoint}")"
+
+    if [ "$(echo "${commit_response_payload}" | jq -r .message)" != "null" ]; then
+      fatal "Error linking the pull request (${error_message})"
+    else
+      comment_url="$(echo "${commit_response_payload}" | jq -r .html_url)"
+      info "Created comment at ${comment_url}"
+    fi
+  else
+    error_message=$(echo "${pr_response_payload}" | jq -r .message)
+    fatal "Error creating pull request (${error_message})"
+  fi
+else
+  info "No change!"
+fi
---
+++ tp7/PC1/node/test-build.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+#
+# Run a test build for all images.
+
+set -euo pipefail
+
+. functions.sh
+
+# Convert comma delimited cli arguments to arrays
+# E.g. ./test-build.sh 8,10 slim,onbuild
+# "8,10" becomes "8 10" and "slim,onbuild" becomes "slim onbuild"
+IFS=',' read -ra versions_arg <<<"${1:-}"
+IFS=',' read -ra variant_arg <<<"${2:-}"
+
+default_variant=$(get_config "./" "default_variant")
+
+function build() {
+  local version
+  local tag
+  local variant
+  local full_tag
+  local path
+  version="$1"
+  shift
+  variant="$1"
+  shift
+  tag="$1"
+  shift
+
+  if [ -z "${variant}" ]; then
+    full_tag="${tag}"
+    path="${version}/${variant}"
+  elif [ "${variant}" = "default" ]; then
+    full_tag="${tag}"
+    path="${version}"
+  else
+    full_tag="${tag}-${variant}"
+    path="${version}/${variant}"
+  fi
+
+  info "Building ${full_tag}..."
+
+  if ! docker build --cpuset-cpus="0,1" -t node:"${full_tag}" "${path}"; then
+    fatal "Build of ${full_tag} failed!"
+  fi
+  info "Build of ${full_tag} succeeded."
+
+  info "Testing ${full_tag}"
+  docker run --rm -v "$PWD/test-image.sh:/usr/local/bin/test.sh" node:"${full_tag}" test.sh "${full_version}"
+}
+
+cd "$(cd "${0%/*}" && pwd -P)" || exit
+
+IFS=' ' read -ra versions <<<"$(get_versions . "${versions_arg[@]}")"
+if [ ${#versions[@]} -eq 0 ]; then
+  fatal "No valid versions found!"
+fi
+
+for version in "${versions[@]}"; do
+  # Skip "docs" and other non-docker directories
+  [ -f "${version}/Dockerfile" ] || [ -a "${version}/${default_variant}/Dockerfile" ] || continue
+
+  tag=$(get_tag "${version}")
+  full_version=$(get_full_version "${version}")
+
+  # Required for chakracore
+  if [ -f "${version}/Dockerfile" ]; then
+    build "${version}" "default" "${tag}"
+  fi
+
+  # Get supported variants according to the target architecture.
+  # See details in function.sh
+  IFS=' ' read -ra variants <<<"$(get_variants "$(dirname "${version}")" "${variant_arg[@]}")"
+
+  for variant in "${variants[@]}"; do
+    # Skip non-docker directories
+    [ -f "${version}/${variant}/Dockerfile" ] || continue
+
+    if [ "${variant}" = "onbuild" ]; then
+      build "${version}" "${default_variant}" "$tag"
+    fi
+
+    build "${version}" "${variant}" "${tag}"
+  done
+
+done
+
+info "All builds successful!"
+
+exit 0
