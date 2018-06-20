99870c6a95aadd2695f4d0e528752113f98919fc
Matias-L <m.lionel.tkd@gmail.com>
2018-06-20 19:00:58 -0300

    Rodri

--- tp7/PC2/rmDGW.sh
+++ tp7/PC2/rmDGW.sh
@@ -6,6 +6,7 @@ docker exec pc2_r6_1 ip -6 route delete default
 docker exec pc2_r8_1 ip -6 route delete default
 
 docker exec pc2_b2_1 ip -6 route delete default
+docker exec pc2_b2_1 ip -6 route add default via 2001::a
 
 docker exec pc2_h12_1 ip -6 route delete default
 docker exec pc2_h14_1 ip -6 route delete default
@@ -16,3 +17,5 @@ docker exec pc2_h14_1 ip -6 route add default via 2001:bbbb:5555::18
 docker exec pc2_r4_1 ip -6 route add default via 2001:bbbb:1111::12
 docker exec pc2_r6_1 ip -6 route add default via 2001:bbbb:2222::12
 docker exec pc2_r8_1 ip -6 route add default via 2001:bbbb:3333::12
+
+
