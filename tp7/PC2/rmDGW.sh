29bfe9e425e46cab3481529411b893be47fe9856
gasparmando <gasparmando@hotmail.com>
2018-06-20 17:35:12 -0300

    Archivos vol

--- tp7/PC2/rmDGW.sh
+++ tp7/PC2/rmDGW.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-docker exec pc1_r3_1 ip -6 route delete default
-docker exec pc1_r5_1 ip -6 route delete default
-docker exec pc1_r7_1 ip -6 route delete default
+docker exec pc2_r4_1 ip -6 route delete default
+docker exec pc2_r6_1 ip -6 route delete default
+docker exec pc2_r8_1 ip -6 route delete default
 
-docker exec pc1_b1_1 ip -6 route delete default
+docker exec pc2_b2_1 ip -6 route delete default
 
-docker exec pc1_h11_1 ip -6 route delete default
-docker exec pc1_h13_1 ip -6 route delete default
+docker exec pc2_h12_1 ip -6 route delete default
+docker exec pc2_h14_1 ip -6 route delete default
 
-docker exec pc1_h11_1 ip -6 route add default via 2001:aaaa:4444::15
-docker exec pc1_h13_1 ip -6 route add default via 2001:aaaa:5555::17
+docker exec pc2_h12_1 ip -6 route add default via 2001:bbbb:4444::16
+docker exec pc2_h14_1 ip -6 route add default via 2001:bbbb:5555::18
 
-docker exec pc1_r3_1 ip -6 route add default via 2001:aaaa:1111::11
-docker exec pc1_r5_1 ip -6 route add default via 2001:aaaa:2222::11
-docker exec pc1_r7_1 ip -6 route add default via 2001:aaaa:3333::11
+docker exec pc2_r4_1 ip -6 route add default via 2001:bbbb:1111::12
+docker exec pc2_r6_1 ip -6 route add default via 2001:bbbb:2222::12
+docker exec pc2_r8_1 ip -6 route add default via 2001:bbbb:3333::12
